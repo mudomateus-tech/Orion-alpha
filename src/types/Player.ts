66301d59cafd6b3f9f6e87d2ab8d8b7e940c4bfc
Matheus <MUDOMATEUS@GMAIL.COM>
2026-08-05 15:30:38 -0400

    Restaura tipo Player

---
+++ src/types/Player.ts
@@ -0,0 +1,39 @@
+export interface Localizacao {
+  latitude: number;
+  longitude: number;
+}
+
+export type Papel =
+  | "agente"
+  | "infiltrado"
+  | "hacker";
+
+export type Tipo =
+  | "comandante"
+  | "agente";
+
+
+export interface Jogador {
+
+  id: string;
+
+  nome: string;
+
+  papel: Papel;
+
+  tipo: Tipo;
+
+  conectado: boolean;
+
+  entrouNaPartida: boolean;
+
+  status: "ativo" | "morto";
+
+  vivo?: boolean;
+
+  localizacao?: Localizacao;
+
+}
+
+
+export type Player = Jogador;
